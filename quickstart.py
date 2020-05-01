37d90a18746eaeef89e440251a23dab421e757d5
Torstein Wilhelmsen Sandvik <MotstandenOkononomi>
2020-05-02 00:21:53 +0200

    wip

--- quickstart.py
+++ quickstart.py
@@ -4,7 +4,14 @@ from bs4 import BeautifulSoup
 def clear_terminal():
     os.system('cls')
 
+def print_soup_props(soup):
+    print(soup.title)
+    
+
 if __name__ == "__main__":
     clear_terminal()
     soup = BeautifulSoup(open("./index.html"), "html.parser")
-    print(soup.prettify())
+    
+    print_soup_props(soup)
+    
+    # print(soup.prettify())
