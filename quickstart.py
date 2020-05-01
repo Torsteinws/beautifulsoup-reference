823af2da589a13273f31b2d15d5df26c14f5413c
Torstein Wilhelmsen Sandvik <MotstandenOkononomi>
2020-05-02 00:16:32 +0200

    setup

--- quickstart.py
+++ quickstart.py
@@ -1,7 +1,10 @@
 import os
+from bs4 import BeautifulSoup
 
 def clear_terminal():
     os.system('cls')
 
 if __name__ == "__main__":
     clear_terminal()
+    soup = BeautifulSoup(open("./index.html"), "html.parser")
+    print(soup.prettify())
